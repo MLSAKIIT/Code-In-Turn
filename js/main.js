74dc9a2deb88ced98e868874512f434dd5e5a74f
adarsh01-debug <adarsh.official2299@gmail.com>
2021-03-05 18:36:41 +0530

    Landing Page initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// NAVBAR
+
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+    });
+}
+
+navSlide()
